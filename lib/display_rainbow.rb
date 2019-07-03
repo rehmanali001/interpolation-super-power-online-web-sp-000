b2b0f4723630010a0c61bea78b772fc3c66fa114
Rehman Ali <rehmanali001@gmail.com>
2019-07-03 19:03:29 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -3,3 +3,7 @@ def print_business_card(person)
   puts "Name: #{person[0]}, Age: #{person[1]}, Occupation: #{person[2]}"
   puts "Contact: #{person[3]}"
 end
+
+bob = ["Bob", 46, "Juggler", "555-555-5555"]
+
+print_business_card(bob)
