345e9d40d2a9824feb4240bc72378a4b289b6d21
Rehman Ali <rehmanali001@gmail.com>
2019-07-03 19:06:27 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,9 +1,1 @@
-# Write your #display_rainbow method here
-def print_business_card(person)
-  puts "Name: #{person[0]}, Age: #{person[1]}, Occupation: #{person[2]}"
-  puts "Contact: #{person[3]}"
-end
-
-bob = ["Bob", 46, "Juggler", "555-555-5555"]
-
-print_business_card(bob)
+def display_rainbow
